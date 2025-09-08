8ed8958f69e9d97a7232127e2f1e4b00b2d4a4f2
Escobar <fabioescobar2008@gmail.com>
2025-09-08 13:24:05 -0300

    feat: added logout button in home screen

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,9 +1,11 @@
+import { AuthStatus } from "@/components/auth/AuthStatus";
+
 export default function HomePage() {
   return (
     <main className="p-6">
       <h1>Home</h1>
+      <br />
+      <AuthStatus />
     </main>
   );
 }
-
-
