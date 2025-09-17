282f4d13b019161e89a28d9c3b07d5d5a1cb41af
Escobar <fabioescobar2008@gmail.com>
2025-09-17 14:25:04 -0300

    docs: clean database script

---
+++ markdowns-plans/sql/delete_script.sql
@@ -0,0 +1,17 @@
+DELETE FROM profiles
+where user_id = '567714b8-87a3-454b-a852-1ac7ee075cb0';
+
+DELETE FROM diet_recommendations
+where user_id = '567714b8-87a3-454b-a852-1ac7ee075cb0';
+
+DELETE FROM user_badges
+where user_id = '567714b8-87a3-454b-a852-1ac7ee075cb0';
+
+DELETE FROM user_current_diet
+where user_id = '567714b8-87a3-454b-a852-1ac7ee075cb0';
+
+DELETE FROM user_metrics
+where user_id = '567714b8-87a3-454b-a852-1ac7ee075cb0';
+
+DELETE FROM weights
+where user_id = '567714b8-87a3-454b-a852-1ac7ee075cb0';
