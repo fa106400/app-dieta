b676b09b84b21f9d6471a0ee206615490d92da9f
Escobar <fabioescobar2008@gmail.com>
2025-09-24 21:05:00 -0300

    fix: removed unused functions

--- src/components/diets/DietCard.tsx
+++ src/components/diets/DietCard.tsx
@@ -62,28 +62,28 @@ export function DietCard({
     }
   };*/
 
-  const getCategoryColor = (category: string | null) => {
+  /*const getCategoryColor = (category: string | null) => {
     switch (category) {
-      // case "low_carb":
-      //   return "bg-blue-100 text-blue-800";
-      // case "keto":
-      //   return "bg-purple-100 text-purple-800";
-      // case "vegetarian":
-      //   return "bg-green-100 text-green-800";
-      // case "balanced":
-      //   return "bg-orange-100 text-orange-800";
+       case "low_carb":
+         return "bg-blue-100 text-blue-800";
+       case "keto":
+         return "bg-purple-100 text-purple-800";
+       case "vegetarian":
+         return "bg-green-100 text-green-800";
+       case "balanced":
+         return "bg-orange-100 text-orange-800";
       default:
         return "bg-gray-100 text-gray-800";
     }
-  };
+  };*/
 
-  const formatCategory = (category: string | null) => {
+  /*const formatCategory = (category: string | null) => {
     if (!category) return "Desconhecida";
     return category
       .split("_")
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
       .join(" ");
-  };
+  };*/
 
   // if (viewMode === "list") {
   //   return (
