2763262dcf067a77bb967ac0d1f9866c5e7cd5f6
Escobar <fabioescobar2008@gmail.com>
2025-10-01 13:38:27 -0300

    docs: novo script de limpeza

--- markdowns-plans/sql/delete_script.sql
+++ markdowns-plans/sql/delete_script.sql
@@ -1,3 +1,4 @@
+--delete for single user - escobar
 DELETE FROM profiles
 where user_id = '567714b8-87a3-454b-a852-1ac7ee075cb0';
 
@@ -15,3 +16,19 @@ where user_id = '567714b8-87a3-454b-a852-1ac7ee075cb0';
 
 DELETE FROM weights
 where user_id = '567714b8-87a3-454b-a852-1ac7ee075cb0';
+
+--create a script to loop through all users and delete their data
+--delete for all users
+do $$
+declare auth_record RECORD;
+begin
+for auth_record in select id from auth.users LOOP
+    update public.profiles 
+      set onboarding_completed = false, estimated_calories = null where user_id = auth_record.id;
+    delete from public.diet_recommendations where user_id = auth_record.id;
+    delete from public.user_current_diet where user_id = auth_record.id;
+    delete from public.user_badges where user_id = auth_record.id;
+    delete from public.user_metrics where user_id = auth_record.id;
+    delete from public.weights where user_id = auth_record.id;
+end loop;
+end $$;
